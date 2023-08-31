dc85ea81d227f261a983102311d35307cb17eeb2
ChrisJosJC <chrisjoseph380gmail.com>
2023-08-31 10:16:55 -0400

    Solving main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material';
 import './index.css'
 
