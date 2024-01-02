d2dd8ff39510b5dd09c0da0f4f67a94e9e20fca9
ChrisJosJC <63082513+ChrisJosJC@users.noreply.github.com>
2024-01-02 16:27:52 -0400

    Fixing fetch to JSON

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -19,7 +19,7 @@ export const useQuestionStore = create<State>()(persist((set, get) => {
         currentQuestion: 0,
 
         requestQuestion: async (limit: number) => {
-            const res = await fetch("http://localhost:5173/data.json")
+            const res = await fetch("/data.json")
             const json: Questions = await res.json()
             const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
 
